61c4668429e0612370a0695da8eb6ae271d746cb
DongJin Kim <31959020+ywydpapa@users.noreply.github.com>
2024-10-23 11:11:38 +0900

    처리속도 증가

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ from dbconn import tradelog
 dotenv.load_dotenv()
 bidcnt = 1
 svrno = os.getenv("server_no")
-mainver = 241014001
+mainver = 241023001
 
 
 def loadmyset(uno):
@@ -754,4 +754,4 @@ while True:
             if cnt > 3600:  # 1시간 마다 재시작
                 cnt = 1
                 service_restart()
-        time.sleep(1)
+        time.sleep(0.5)
